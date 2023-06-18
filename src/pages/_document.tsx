933bc1b35ca198985aede5260615793160769d44
devadaviddd <s3927112@rmit.edu.vn>
2023-06-18 20:32:28 +0700

    meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="mobile-web-app-capable" content="yes"/>
+
       </Head>
       <body>
         <Main />
